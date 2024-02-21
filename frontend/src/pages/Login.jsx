d29d4c7f3f7c60ba8a112060539e5dce15fa33ae
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-02-21 11:19:06 +1000

    updated placeholder logo with actual logo

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,12 +2,13 @@
 // import Nav from "../components/Nav"
 // import Footer from "../components/Footer"
 import { Link } from "react-router-dom"
+import logo from "../assets/img/hs-high-resolution-logo-transparent.svg"
 
 function Login() {
     return <>
         {/* <Header /> */}
         <div>
-            <img src="https://picsum.photos/100/100" alt="logo image placeholder" className="my-4 mx-auto rotate-45" />
+            <img src={logo} alt="logo image" className="w-40 my-4 mx-auto" />
             <h1>This is the login page!</h1>
             <form className="flex flex-col gap-y-2">
                 <input type="email" placeholder="Email" className="input input-bordered" />
