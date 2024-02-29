c79d1b1a87115adbe537cd69b87881a97c0668a5
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-02-29 14:26:32 +1000

    modified to use dotenv & .env file

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,10 +1,16 @@
 import mysql from "mysql2/promise"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 export const db = mysql.createPool({
-    host: "localhost",
-    user: "root",
-    password: "root",
-    database: "high-street-gym"
+    // host: "localhost",
+    // user: "root",
+    // password: "root",
+    // database: "high-street-gym"
 
     // TODO: Revise above using .env file
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 })
