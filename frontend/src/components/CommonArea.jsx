a2949e6cd87f3d810dc11b039b5c1009479d3cd7
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-03-05 13:59:23 +1000

    Updated with conditional rendering using auth info not logged in: show footer logged in: show nav bar

--- frontend/src/components/CommonArea.jsx
+++ frontend/src/components/CommonArea.jsx
@@ -1,22 +1,24 @@
+import { Outlet } from "react-router-dom"
+import { useAuthentication } from "../hooks/authentication"
 import Header from "./Header"
 import Nav from "./Nav"
 import Footer from "./Footer"
-import { Outlet } from "react-router-dom"
-import { useState } from "react"
 
 function CommonArea () {
-    const [isLoggedIn, setIsLoggedIn] = useState(true)
+    const [user, login, logout] = useAuthentication()
 
-    // setIsLoggedIn(false)    // test code
-    
+    const loggedInUser = user ? "Logged in as: " + user.firstName + " " + user.lastName + " @" + user.role : "Not logged in yet!"
+    // const loggedInUser = user && "Logged in as: " + user.firstName + " " + user.lastName + " @" + user.role
+
     return <>
         <Header />
+        <span className="text-red-600 ">{loggedInUser}</span>
         <div className="pb-16"> {/* bottom padding to avoid main content from overlapping with bottom nav bar */}
             <Outlet />
         </div>
-        {/* <Nav /> */}
-        {/* <Footer /> */}
-        { isLoggedIn ? <Nav /> : <Footer /> }
+        {
+            user ? <Nav /> : <Footer />
+        }
     </>
 }
 
