e5aec553bdc25016cc17d1f0fd2d88ed8fb1f3f6
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-03-05 14:02:48 +1000

    added logout function & conditional rendering Logout: use logout function of authentication & navigate to home afterwords XML Import: only appears for admin & trainer role

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,12 +1,22 @@
-import { NavLink } from "react-router-dom"
+import { NavLink, useNavigate } from "react-router-dom"
+import { useAuthentication } from "../hooks/authentication"
 import timetable from "../assets/img/iconmonstr-calendar-4.svg"
 import bookings from "../assets/img/iconmonstr-list-lined.svg"
 import blog from "../assets/img/iconmonstr-script-5.svg"
 import profile from "../assets/img/iconmonstr-user-circle-thin.svg"
 import upload from "../assets/img/iconmonstr-upload-18.svg"
-import logout from "../assets/img/iconmonstr-log-out-16.svg"
+import logoutIcon from "../assets/img/iconmonstr-log-out-16.svg"
 
 function Nav() {
+    const [user, login, logout] = useAuthentication()
+    const navigate = useNavigate()
+
+    // Define logout procedure
+    function onLogoutClick(e) {
+        logout()
+        navigate("/")
+    }
+
     return <nav className="btm-nav">
         <NavLink to="/timetable" // className="btn-ghost"
             // className={({isActive}) => isActive ? "active" : ""}
@@ -34,16 +44,23 @@ function Nav() {
             <img src={profile} alt="person torso icon" className="h-5 w-5" />
             <span className="btm-nav-label text-xs text-center">Profile</span>
         </NavLink>
-        <NavLink to="/import" // className="btn-ghost"
-            // className={({isActive}) => isActive ? "active" : ""}
-        >
-            <img src={upload} alt="file uploading icon" className="h-5 w-5" />
-            <span className="btm-nav-label text-xs text-center">XML Import</span>
-        </NavLink>
-        <NavLink to="/">
-            <img src={logout} alt="exit from door icon" className="h-5 w-5" />
+
+        {
+            user && (user.role == "admin" || user.role == "trainer")
+            // user && (user.role == "trainer") // test code to see if it disappears
+                ? <NavLink to="/import" // className="btn-ghost"
+                    // className={({isActive}) => isActive ? "active" : ""}
+                >
+                    <img src={upload} alt="file uploading icon" className="h-5 w-5" />
+                    <span className="btm-nav-label text-xs text-center">XML Import</span>
+                </NavLink>
+                : <></>
+        }
+
+        <a onClick={onLogoutClick}>
+            <img src={logoutIcon} alt="exit from door icon" className="h-5 w-5" />
             <span className="btm-nav-label text-xs text-center">Logout</span>
-        </NavLink>
+        </a>
     </nav>
 }
 
