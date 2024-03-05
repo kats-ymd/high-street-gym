fd5349d7281622a956f7485b6a89118ad9b1638a
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-03-05 14:01:20 +1000

    Updated with daisyUI footer component

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,13 @@
 function Footer() {
-    return <footer className="bg-red-600 font-bold text-white">
-        <span>2023 &copy; High Street Gym</span>
+    // return <footer className="bg-red-600 font-bold text-white">
+    //     <span>2023 &copy; High Street Gym</span>
+    // </footer>
+
+    return <footer className="footer footer-center p-4 bg-base-300 text-base-content">
+        <aside>
+            {/* <p>Copyright © 2024 - All right reserved by ACME Industries Ltd</p> */}
+            <p>2023 &copy; High Street Gym</p>
+        </aside>
     </footer>
 }
 
