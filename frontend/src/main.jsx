3c06b2246a4e8b7608917c60a6b125ceba93aae2
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-03-05 12:37:20 +1000

    Added <AuthenticationProvider>

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import router from './router.jsx'
+import { AuthenticationProvider } from './hooks/authentication.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <AuthenticationProvider router={router}>
+      <RouterProvider router={router} />
+    </AuthenticationProvider>
   </React.StrictMode>
 )
