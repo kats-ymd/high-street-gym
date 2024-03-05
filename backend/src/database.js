0d98a14e8a6c762ab20566d5056ba3356cde3ead
Kats Yamada <xyzkats+gitlab@gmail.com>
2024-03-05 13:57:41 +1000

    Temp. revert back to hard-coded db info

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,14 +3,16 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 export const db = mysql.createPool({
-    // host: "localhost",
-    // user: "root",
-    // password: "root",
-    // database: "high-street-gym"
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "high-street-gym"
 
-    // TODO: Revise above using .env file
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    // // TODO: Revise above using .env file
+    // // Below code worked for testing model function individually,
+    // // but did not work for actual api call...
+    // host: process.env.DB_HOST,
+    // user: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // database: process.env.DB_DATABASE
 })
