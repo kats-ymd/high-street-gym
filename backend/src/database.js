08c9e4b2c463b5c6f0cbe96be44a5253289f7e2b
Kats Yamada <140475065+kats-ymd@users.noreply.github.com>
2024-05-02 06:47:48 +1000

    updated comments

--- backend/src/database.js
+++ backend/src/database.js
@@ -3,20 +3,17 @@ import * as dotenv from "dotenv"
 dotenv.config()
 
 export const db = mysql.createPool({
-    // host: "localhost",
-    // user: "root",
-    // password: "root",
-    // database: "high-street-gym"
-
-    // if .env file is placed under app root folder,
-    // it works for app but does not work for model function only test
-    // if placed under backend folder,
-    // it works for model function only test but does not for app
-    //
-    // TODO: Investigate the difference
-
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE
 })
+
+
+// if .env file is placed under app root folder,
+// it works for app but does not work for model function only test
+// if placed under backend folder,
+// it works for model function only test but does not for app
+//
+// >>> looks like it depends on what directory you are in
+// >>> if running model functions, move to backend directory
