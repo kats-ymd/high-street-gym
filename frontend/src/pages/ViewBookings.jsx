c34ce53c6d9e9900f3c239b4a40c46f0e115ff33
Kats Yamada <140475065+kats-ymd@users.noreply.github.com>
2024-05-28 10:46:38 +1000

    updated booking list & implemented cancel function

--- frontend/src/pages/ViewBookings.jsx
+++ frontend/src/pages/ViewBookings.jsx
@@ -32,16 +32,29 @@ function ViewBookings () {
         }
     }, [user])
 
+    function handleCancelOnClick (booking) {
+        console.log(booking.booking_id)
+        const deleteConfirmationMessage = `Are you sure to cancel this class?`
+            + `\n\n${booking.activity_name}`
+            + `\nwith ${booking.trainer_first_name} ${booking.trainer_last_name}`
+            + `\n@${booking.location_name}`
+            + `\non ${new Date(booking.class_date).toLocaleDateString()} ${booking.class_time}`
+
+        if (window.confirm(deleteConfirmationMessage)) {
+            Bookings.deleteByID(booking.booking_id, user.authenticationKey).then(result => {
+                console.log(result)
+            })
+        }
+    }
+
     return loading ? <LoadingSpinner /> :
         allClasses ? <>
             <div className="flex flex-col">
-                <h1>Your scheduled classes:</h1>
+                <h1 className="text-2xl">Your scheduled classes as trainer:</h1>
                 {allClasses.map((eachBooking) =>
-                    <div key={eachBooking.class_id} className="border border-black py-2">
+                    <div key={eachBooking.class_id} className="border-t-2 border-dashed border-gray-400 py-2">
                         <div className="flex justify-between">
                             <span className="">{eachBooking.activity_name}</span>
-                            <span className="">as trainer</span>
-                            <span></span>
                         </div>
                         <div className="flex justify-between">
                             <span>@{eachBooking.location_name}</span>
@@ -50,17 +63,24 @@ function ViewBookings () {
                     </div>
                 )}
             </div>
-        </> :
-        !allBookings ? <>You have no classes booked yet!</> :
-        <>
+        </>
+        : !allBookings ? <>
+            <div className="text-2xl">You have no classes booked yet!</div>
+        </>
+        : <>
             <div className="flex flex-col">
-                <h1>Your current class bookings:</h1>
+                <h1 className="text-2xl">Your current class bookings:</h1>
                 {allBookings.map((eachBooking) =>
-                    <div key={eachBooking.booking_id} className="border border-black py-2">
+                    <div key={eachBooking.booking_id} className="border-t-2 border-dashed border-gray-400 py-2">
                         <div className="flex justify-between">
-                            <span className="">{eachBooking.activity_name}</span>
-                            <span className="">with {eachBooking.trainer_first_name} {eachBooking.trainer_last_name}</span>
-                            <span></span>
+                            <span className="basis-1/3">{eachBooking.activity_name}</span>
+                            <span className="basis-1/3">with {eachBooking.trainer_first_name} {eachBooking.trainer_last_name}</span>
+                            <button
+                                onClick={() => handleCancelOnClick(eachBooking)}
+                                className="basis-1/3 text-end text-red-600"
+                            >
+                                Cancel X
+                            </button>
                         </div>
                         <div className="flex justify-between">
                             <span>@{eachBooking.location_name}</span>
