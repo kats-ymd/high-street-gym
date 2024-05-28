ca06e1a816ba4ec404c578b970b601c386b56ba9
Kats Yamada <140475065+kats-ymd@users.noreply.github.com>
2024-05-28 10:38:11 +1000

    added getByActivityID

--- frontend/src/api/classes.js
+++ frontend/src/api/classes.js
@@ -1,7 +1,27 @@
 import { API_URL } from "./api"
 
+export async function getByActivityID(activityID, authenticationKey) {
+    // GET from the API /classes
+    const response = await fetch(
+        API_URL + "/classes/" + activityID,
+        {
+            method: "GET",
+            headers: {
+                'Content-Type': "application/json",
+                'X-AUTH-KEY': authenticationKey
+            },
+        }
+    )
+
+    const APIResponseObject = await response.json()
+
+    console.log(APIResponseObject)
+
+    return APIResponseObject
+}
+
 export async function getAll(authenticationKey) {
-    // GET from the API /users
+    // GET from the API /classes
     const response = await fetch(
         API_URL + "/classes",
         {
