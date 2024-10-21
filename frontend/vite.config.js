175632e0c706b776f90dfe0c2d9df87f30d776ec
Kats Yamada <140475065+kats-ymd@users.noreply.github.com>
2024-10-21 13:21:51 +1000

    added server setting to allow access from other than localhost

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',  // Bind to all network interfaces
+    port: 5173        // Port number, can be any available port
+  }
 })
